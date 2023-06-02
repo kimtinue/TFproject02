40da1a4f6e842e7936be1dfee1b40a4f54a317b8
kimtinue <kimtinue0@gmail.com>
2023-06-02 17:45:25 +0900

    Project Setting

---
+++ src/main/java/com/board/TFproject02Application.java
@@ -0,0 +1,13 @@
+package com.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TFproject02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TFproject02Application.class, args);
+	}
+
+}
